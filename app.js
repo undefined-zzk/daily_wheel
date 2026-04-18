06ca68ca4cbc9c8e4266cef2eadfbfbfff83dc45
jackal <2143511430@qq.com>
2026-04-18 12:05:24 +0800

    检查更新

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 // app.js
 App({
   onLaunch() {
-
+    // 检查小程序更新
+    this.checkUpdate()
   },
+
+  // 检查小程序更新
+  checkUpdate() {
+    const updateManager = wx.getUpdateManager()
+
+    // 检查是否有新版本
+    updateManager.onCheckForUpdate((res) => {
+      console.log('检查更新结果:', res.hasUpdate)
+    })
+
+    // 新版本下载成功
+    updateManager.onUpdateReady(() => {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: (res) => {
+          if (res.confirm) {
+            // 新版本已下载，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    // 新版本下载失败
+    updateManager.onUpdateFailed(() => {
+      wx.showModal({
+        title: '更新失败',
+        content: '新版本下载失败，请删除小程序后重新搜索打开',
+        showCancel: false
+      })
+    })
+  }
 })
